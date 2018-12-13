1f41b5c728a5464c093883bf52b53ad719d22771
Ben Kemp <benkemp@bens-mbp.lan>
2018-12-13 16:47:37 +0100

    Initial Commit

---
+++ tlnt001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tlnt001
+//
+//  Created by Ben Kemp on 12/13/18.
+//  Copyright © 2018 Talento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
