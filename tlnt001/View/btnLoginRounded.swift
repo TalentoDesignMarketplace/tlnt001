be7b8077e035c647622d17c4cfc89154d208c4c9
Ben Kemp <benkemp@bens-mbp.lan>
2018-12-15 09:42:48 +0100

    Initial Commit

---
+++ tlnt001/View/btnLoginRounded.swift
@@ -0,0 +1,13 @@
+//
+//  btnLoginRounded.swift
+//  tlnt001
+//
+//  Created by Ben Kemp on 12/13/18.
+//  Copyright © 2018 Talento. All rights reserved.
+//
+
+import UIKit
+
+class btnLoginRounded: UIView {
+    
+}
